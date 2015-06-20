657bb1efe1d3c9291363ccea98950d10537ce30d
pezy <urbancpz@gmail.com>
2015-06-20 12:57:21 +0800

    fixed ex9_22.cpp bug
    - TestCase: `1 1 1 1 6 7 8 9`
    - old formation world be crashed, cause `iter` will jump over the `mid`.
    - the other problem is the `mid` always changed. Thus it must be different with the origin `mid`.

--- ex9_22.cpp
+++ ex9_22.cpp
@@ -1,16 +1,16 @@
 #include <iostream>
 #include <vector>
-
-using namespace std;
+using std::vector;
 
 void insertDoubleValue(vector<int> &iv, int some_val)
 {
-    auto iter = iv.begin(), mid = iv.begin() + iv.size()/2;
+    auto cursor = iv.size() / 2;
+    auto iter = iv.begin(), mid = iv.begin() + cursor;
     while (iter != mid) {
         if (*iter == some_val) {
             iter = iv.insert(iter, 2 * some_val);
-            ++iter;
-            mid = iv.begin() + iv.size()/2;
+            ++iter; ++cursor;
+            mid = iv.begin() + cursor;
         }
         ++iter;
     }
@@ -19,13 +19,13 @@ void insertDoubleValue(vector<int> &iv, int some_val)
 void print(const vector<int> &iv)
 {
     for (auto i : iv)
-        cout << i << " ";
-    cout << endl;
+        std::cout << i << " ";
+    std::cout << std::endl;
 }
 
 int main()
 {
-    vector<int> iv = {1, 2, 1, 3, 1, 4, 1, 5};
+    vector<int> iv = {1, 1, 1, 1, 1, 7, 1, 9};
     insertDoubleValue(iv, 1);
     print(iv);
 }
